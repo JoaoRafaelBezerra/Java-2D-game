bb15ca524e63a93c8cc38786ffdebe14e3819082
Rafael Bezerra <jbezerra@faeterj-petropolis.edu.br>
2023-05-28 22:00:39 -0300

    Making game loop

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -2,21 +2,20 @@ package Program;
 
 import javax.swing.JFrame;
 
-import Window.GamePanel;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		JFrame window = new JFrame();
+		GamePanel gamePanel = new GamePanel();
+		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Close the window properly close
 		window.setResizable(false);
 		window.setTitle("Generic title");
 		window.setLocationRelativeTo(null);//Display the window on the center of the screen by not specifying the location
 		window.setVisible(true);
-		
-		GamePanel gamePanel = new GamePanel();
-		
 		window.add(gamePanel);
 		window.pack();
 		
